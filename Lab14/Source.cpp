1036af445fb6d7faaaf9baf9025f870f5f880821
Igor Semikin <i.semikin@list.ru>
2022-04-25 13:21:27 +0300

    Добавьте файлы проекта.

---
+++ Lab14/Source.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+
+/*Задание:
+Ввести некоторое число и записать его цифры в стек.Вывести число, у которого цифры идут в обратном порядке.
+Цифры из записи числа считывать поочередно(например, с помощью getch()).
+Вычислить наименьшую и наибольшую цифры в записи числа.*/
+
+struct Element
+{
+	int data;
+	Element* next = NULL;
+};
+
+struct Stack
+{
+	Element* head = NULL;
+};
+
+void push(Stack& s, int d)
+{
+	Element* e = (Element*)malloc(sizeof(Element));
+	e->data = d;
+	e->next = s.head;
+	s.head = e;
+}
+
+int pull(Stack& s)
+{
+	int d = s.head->data;
+	Element* e = s.head;
+	s.head = s.head->next;
+	free(e);
+	return d;
+}
+
+void printStack(Stack& s)
+{
+
+}
+
+int main()
+{
+
+
+
+	return 0;
+}
